7d17105cfa045181044085f4743205182ee71cf9
Your Name <you@example.com>
2021-12-02 14:21:12 +0000

    commit to clear compile errors

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import "./App.scss";
 import Header from "./Components/Header/Header.jsx";
 import CardContainer from "./Components/CardContainer/CardContainer.jsx";
-import EmployeeContainer from "./Components/EmployeeContainer/EmployeeContainer"
 
 
 function App() {
